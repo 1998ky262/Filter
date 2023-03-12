8deb3e56234c016a8424e98cbbd64bb719276371
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-03-12 20:23:16 +0900

    Update block.js

--- block.js
+++ block.js
@@ -2,16 +2,19 @@ async function gets() {
 	var now = await Date.now();
 	var res = await fetch(`https://croud7.github.io/Filter/extension/list.json?${now}`);
 	var res = await res.json();
-	ver info = await fetch("https://1998ky262.github.io/filter/info.json");
-	ver info = await info.json();
+	var info = await fetch("https://1998ky262.github.io/filter/info.json");
+	var info = await info.json();
 	await goes(res);
 }
 
 async function goes(text) {
 	var leng = await text.length;
+	if (info[0]=="ture"){
+		location.href = "https://croud7.github.io/Filter/";
+	}
 	for(c = 0; leng > c; c++){
 		if(location.hostname == text[c]){
-			location.href = "https://croud7.github.io/Filter/"
+			location.href = "https://croud7.github.io/Filter/";
 		}
 	}
 }
