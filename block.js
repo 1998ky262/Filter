aeadb7e35cef37163df34868448837be3abe0dd3
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-03-12 20:17:09 +0900

    Add files via upload

---
+++ block.js
@@ -0,0 +1,23 @@
+async function gets() {
+	var now = await Date.now();
+	var res = await fetch(`https://croud7.github.io/Filter/extension/list.json?${now}`);
+	var res = await res.json();
+	await goes(res);
+}
+
+async function goes(text) {
+	var leng = await text.length;
+	for(c = 0; leng > c; c++){
+		if(location.hostname == text[c]){
+			location.href = "https://croud7.github.io/Filter/"
+		}
+	}
+}
+
+gets();
+
+/* 30秒ごとにブロックリストに入っているかどうかを確認 */
+var interval = () => {
+	gets();
+}
+setInterval(interval, 30000);
