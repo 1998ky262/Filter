9ead85237060eed32d14522ffb5382802d07646c
1998fx <106678831+1998ky262@users.noreply.github.com>
2023-03-13 01:20:12 +0900

    Update block.js

--- block.js
+++ block.js
@@ -10,7 +10,10 @@ async function gets() {
 async function goes(text,text2) {
 	var leng = await text.length;
 	if (text2[0]=="true"){
-		location.href = "https://croud7.github.io/Filter/";
+		if(location.hostname !== "croud7.github.io){
+		   // 繰り返しを防ぐ
+		   location.href = "https://croud7.github.io/Filter/";
+		   }
 	}
 	for(c = 0; leng > c; c++){
 		if(location.hostname == text[c]){
